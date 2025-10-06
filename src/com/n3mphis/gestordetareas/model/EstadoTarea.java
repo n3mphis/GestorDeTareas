346b1c5d7cae92efe4fb2f39a4bb3acb471fbf90
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-06 13:03:48 -0300

    Agregado el ENUM EstadoTarea (por default en pendiente) Y tambien agregue el constructor, getters, toString, equals y hashcode a la clase Tarea

---
+++ src/com/n3mphis/gestordetareas/model/EstadoTarea.java
@@ -0,0 +1,6 @@
+package com.n3mphis.gestordetareas.model;
+
+public enum EstadoTarea {
+    PENDIENTE,
+    COMPLETADO
+}
--- src/com/n3mphis/gestordetareas/model/Tarea.java
+++ src/com/n3mphis/gestordetareas/model/Tarea.java
@@ -1,4 +1,45 @@
 package com.n3mphis.gestordetareas.model;
 
+import java.util.Objects;
+
 public class Tarea {
+    private String descripcion;
+    private EstadoTarea estado;
+    private Long id;
+
+    public Tarea(String descripcion, Long id) {
+        this.descripcion = descripcion;
+        this.id = id;
+        this.estado = EstadoTarea.PENDIENTE;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public EstadoTarea getEstado() {
+        return estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String toString() {
+        return "Descripción de la tarea: " + descripcion +"\n" +
+                "Estado: " + estado + "\n" +
+                "ID: " + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarea tarea = (Tarea) o;
+        return Objects.equals(descripcion, tarea.descripcion) && estado == tarea.estado && Objects.equals(id, tarea.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(descripcion, estado, id);
+    }
 }
