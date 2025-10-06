c4220142b04cdb6f16658a0bd307b2dfab1c5367
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-06 14:37:12 -0300

    Agregue los siguientes metodos: obtenerTodasLasTareas, eliminarTarea, marcarTareaCompletada y modificarDescripcion

--- src/com/n3mphis/gestordetareas/service/GestorDeTareas.java
+++ src/com/n3mphis/gestordetareas/service/GestorDeTareas.java
@@ -1,7 +1,10 @@
 package com.n3mphis.gestordetareas.service;
 
+import com.n3mphis.gestordetareas.model.EstadoTarea;
 import com.n3mphis.gestordetareas.model.Tarea;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,5 +23,30 @@ public class GestorDeTareas {
         return tarea;
     }
 
+    public Collection<Tarea> obtenerTodasLasTareas() {
+        return new ArrayList<>(this.tareas.values());
+    }
 
+    public boolean eliminarTarea(Long id) {
+        return tareas.remove(id) != null;
+    }
+
+    public boolean marcarTareaCompletada(Long id) {
+        Tarea tarea = tareas.get(id);
+
+        if (tarea != null && tarea.getEstado() != EstadoTarea.COMPLETADO) {
+            tarea.setEstado(EstadoTarea.COMPLETADO);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean modificarDescripcion(Long id, String descripcion) {
+        Tarea tarea = tareas.get(id);
+        if (tarea != null) {
+            tarea.setDescripcion(descripcion);
+            return true;
+        }
+        return false;
+    }
 }
